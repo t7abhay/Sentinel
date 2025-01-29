00c74862f562cdce47355ec330c37af439587515
t7abhay <abhaytaras18@gmail.com>
2025-01-29 15:57:36 +0530

    add rate limiting

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ import { connectDB } from "./config/DB/dbConnection.js";
 import authRoutes from "./routes/auth/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import errorMiddleware from "./middlewares/errorMiddleware";
-
+import { rateLimit } from "express-rate-limit";
 const app = express();
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    message: "Rate limited",
+});
+
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(limiter);
 app.use(express.json());
 
 // Routes
