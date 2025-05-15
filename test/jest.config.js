7a6deff59a1a38d9ad7564ece62bba02d13768a8
Cm_z1K0=023 <abhaytaras18@gmail.com>
2025-05-15 00:56:43 -0700

    Delete test directory

--- test/jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-export default {
-    testEnvironment: "node",
-    transform: {}, // disable babel since Node handles ESM
-    extensionsToTreatAsEsm: [".js"],
-    moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1",
-    },
-};
